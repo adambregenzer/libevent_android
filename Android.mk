0350608c5c7c33b15c55f939544b54031fbb27b7
Adam Bregenzer <adam@bregenzer.net>
2012-06-19 01:18:59 -0400

    Added android makefile.

---
+++ Android.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:= libevent_full
+LOCAL_MODULE_TAGS:= optional
+
+LOCAL_SRC_FILES :=                      \
+    buffer.c                            \
+    bufferevent.c                       \
+    bufferevent_filter.c                \
+    bufferevent_pair.c                  \
+    bufferevent_ratelim.c               \
+    bufferevent_sock.c                  \
+    epoll.c                             \
+    evdns.c                             \
+    event.c                             \
+    event_tagging.c                     \
+    evmap.c                             \
+    evrpc.c                             \
+    evthread.c                          \
+    evutil.c                            \
+    evutil_rand.c                       \
+    http.c                              \
+    listener.c                          \
+    log.c                               \
+    poll.c                              \
+    select.c                            \
+    signal.c                            \
+    strlcopy.c
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include/
+
+include $(BUILD_STATIC_LIBRARY)
+
